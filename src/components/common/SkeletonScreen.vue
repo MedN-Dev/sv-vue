a86f20c2df8bdcb877f11328b12c972ecc373219
Turkyden <616287091@qq.com>
2018-07-23 23:50:02 +0800

    Add skeleton screen

---
+++ src/components/common/SkeletonScreen.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="sv-skeletonScreen">
+        <content-placeholders>
+            <content-placeholders-heading :img="true" />
+            <content-placeholders-text :lines="3" />
+        </content-placeholders>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
--- src/pages/Energy.vue
+++ src/pages/Energy.vue
@@ -2,11 +2,16 @@
   <div class="sv-page-energy">
     <h3>页面：Energy page </h3>
     <h3>类别: {{category}}</h3>
+    <sv-skeletonScreen></sv-skeletonScreen>
   </div>
 </template>
 <script>
+  import SVSkeletonScreen from '../components/common/SkeletonScreen.vue'
   export default {
-    props: ['category']
+    props: ['category'],
+    components: {
+      'sv-skeletonScreen': SVSkeletonScreen
+    }
   }
 </script>
 <style>
