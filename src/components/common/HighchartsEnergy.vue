11dc57de9eace1554a3dcf60da8004452d6108db
登举 邓 <wj871287@gmail.com>
2018-08-20 13:13:48 +0800

    update overfix

--- src/components/common/HighchartsEnergy.vue
+++ src/components/common/HighchartsEnergy.vue
@@ -53,9 +53,9 @@
         this.xAxis = sun.map(item => item.dateTime);
         // 获取数据数组
         this.sun = sun.map(item => item.value);
-        this.sunEstimation = [[0, sunEstimation[0].value], [this.xAxis.length, sunEstimation[0].value]];
+        this.sunEstimation = [[0, sunEstimation[0].value], [this.xAxis.length-1, sunEstimation[0].value]];
         this.yield = data['yield'].map(item => item.value);
-        this.yieldEstimation = [[0, yieldEstimation[0].value], [this.xAxis.length, yieldEstimation[0].value]];
+        this.yieldEstimation = [[0, yieldEstimation[0].value], [this.xAxis.length-1, yieldEstimation[0].value]];
         // 曲线图表
         this.totalSun = totalSun.map(item => item.value);
         this.totalYield = totalYield.map(item => item.value);
@@ -111,7 +111,7 @@
       // 绘制图表， 挂载id, 横轴坐标数组，数据组
       drawChart(ID, XAXIS, SERIES) {
         // 实际发电量预测与日射量预测
-        let { sunEstimation, yieldEstimation } = this;
+        const { sunEstimation, yieldEstimation } = this;
         Highcharts.setOptions(HighchartsTheme);
         Highcharts.chart(ID, {
           title: { text: '発電実績', },
@@ -127,37 +127,35 @@
             enabled: true,
           },
           yAxis: [{
-              lineWidth: 1,
-              title: {
-                enabled: 'true',
-                text: 'KWh',
-                align:'high',
-                rotation: 0,
-                y: -15,
-                offset: 0,
-                margin:'10px',
-                style:{
-                  'font-size':'14px',
-                }
-		        	}
-            },
-            { 
-              lineWidth: 1,
-              title: {
-                enabled: 'true',
-                align:'high',
-                text: 'KWh/m2',
-                rotation: 0,
-                y: -15,
-                x:0,
-                offset: -25,
-			          style:{
-                  'font-size':'14px',
-                }
-			        },
-              opposite: true,
+            lineWidth: 1,
+            title: {
+              enabled: 'true',
+              text: 'KWh',
+              align:'high',
+              rotation: 0,
+              y: -15,
+              offset: 0,
+              margin:'10px',
+              style:{
+                'font-size':'14px',
+              }
             }
-          ],
+          }, { 
+            lineWidth: 1,
+            title: {
+              enabled: 'true',
+              align:'high',
+              text: 'KWh/m2',
+              rotation: 0,
+              y: -15,
+              x:0,
+              offset: -25,
+              style:{
+                'font-size':'14px',
+              }
+            },
+            opposite: true,
+          }],
           tooltip: {
             borderColor:null,
             borderWidth: 0,
@@ -170,7 +168,6 @@
             formatter: function () {
               let ydPe = this.points[0].y / yieldEstimation[0][1] * 100;
               let sunPe = this.points[1].y / sunEstimation[0][1] * 100;
-              console.log(this)
               return `<table style="border:1px solid #FF6D6C;text-align:center;padding:4px;width:100px;background:#2c303b">
                         <thead style="color:#676C8A">
                           <tr><th>${this.x}日</th><th style="text-align: right">発電量</th><th style="text-align: right">日射量</th></tr>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+    // 生产部署路径
+    baseUrl: process.env.NODE_ENV === 'production'
+    ? '/production-sub-path/'
+    : '/',
     // 设置开发服务器代理
     devServer: {
         proxy: 'http://localhost:8080',
